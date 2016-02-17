312fef5e9b6c7fa248337ed5c92ecc709d132f7a
Ray Borenstein <RayBor@users.noreply.github.com>
2016-02-17 15:15:07 +0100

    Add comments to PN532.h

--- KeyDuino/PN532.h
+++ KeyDuino/PN532.h
@@ -1,3 +1,7 @@
+//This class is only useful for using the NDEF library, that require a PN532 class.
+//We've chosen to do this in order to use Don's NDEF library without having to modify it or integrate it in KeyDuino library.
+//Basically, this class creates a normal KeyDuin object. That's all.
+
 #ifndef __PN532_H__
 #define __PN532_H__
 #include <KeyDuino.h>
