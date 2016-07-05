859c6fd3714b6e6caab054bb489021b9d1a03430
Ray Borenstein <rayborenstein@gmail.com>
2016-07-05 16:30:15 +0200

    Solve problem with P2P : with some device, it worked only with debug mode ... that may be due to a need for delay.

--- KeyDuino/KeyDuino_debug.h
+++ KeyDuino/KeyDuino_debug.h
@@ -6,15 +6,17 @@
 #include "Arduino.h"
 
 #ifdef DEBUG
-#define DMSG(args...)       Serial.print(args)
-#define DMSG_STR(str)       Serial.println(str)
-#define DMSG_HEX(num)       Serial.print(' '); Serial.print(num, HEX)
-#define DMSG_INT(num)       Serial.print(' '); Serial.print(num)
-#else
-#define DMSG(args...)
-#define DMSG_STR(str)
-#define DMSG_HEX(num)
-#define DMSG_INT(num)
+    #define DMSG(args...)       Serial.print(args)
+    #define DMSG_STR(str)       Serial.println(str)
+    #define DMSG_HEX(num)       Serial.print(' '); Serial.print(num, HEX)
+    #define DMSG_INT(num)       Serial.print(' '); Serial.print(num)
+
+#else //Add delay to let time for the process to complete ...
+    #define DELAY_DURATION      10
+    #define DMSG(args...)       delay(DELAY_DURATION)
+    #define DMSG_STR(str)       delay(DELAY_DURATION)
+    #define DMSG_HEX(num)       delay(DELAY_DURATION)
+    #define DMSG_INT(num)       delay(DELAY_DURATION)
 #endif
 
 #endif
